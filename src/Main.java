e1015c22aff51997d5cf263dbc42b4246d32edf7
EkaterinaPlatonova <katkutia@bk.ru>
2024-01-12 19:38:53 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 15_555;
+        int miles = price / 20;
+        System.out.println("Бонус: " + miles);
+    }
+}
